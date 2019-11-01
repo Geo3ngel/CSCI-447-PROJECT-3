329ffe769413ac8f8a9760ddc8dfeb9ca3a1954c
Dana Parker <danaharmonparker@gmail.com>
2019-11-01 17:56:46 -0600

    Add FFNN imports and __init__

--- src/FFNN.py
+++ src/FFNN.py
@@ -1,6 +1,41 @@
-# The Feed Forward Neural Network
+# ----------------------------------------------
+# @file    ffnn.py
+# @authors Dana Parker, Henry Soule, Troy Oster, George Engel
 
+# ----------------------------------------------
+# Standard-library imports
+import random
+import json
+
+# ----------------------------------------------
+# Third-party imports
+import numpy as np
+
+# ----------------------------------------------
+# Custom imports
+import Cost_Functions as cost
+
+# ----------------------------------------------
+# The Feed-Forward Neural Network object
 class FFNN():
     
-    def __init__():
-        print("Setting up Feed Forward Neural Network")
+    # ----------------------------------------------
+    # Constructor
+    #
+    # in_layer_sizes    Contains the number of neurons
+    #                   in the i-th layer
+    def __init__(self, layer_sizes):
+        self.num_layers = len(layer_sizes)
+        self.layer_sizes = layer_sizes
+
+        # Initializes weights via a normal distribution.
+        self.weights = [np.random.randn(x, y)
+            for y, x in zip(layer_sizes[:-1], layer_sizes[1:])]
+
+        # Initializes biases via a normal distribution.
+        #
+        # We don't put a bias on the weights between
+        # the input layer and the first layer,
+        # hence layer_sizes[1:].
+        self.biases = [np.random.randn(x, 1)
+            for x in self.layer_sizes[1:]]
