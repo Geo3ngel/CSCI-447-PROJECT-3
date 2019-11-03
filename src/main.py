17547883db7bea992d9efeec5cbb88baa1d8d36f
Henry Soule <hsoule427@gmail.com>
2019-11-03 11:59:02 -0700

    Added my updates to main.py

--- src/main.py
+++ src/main.py
@@ -94,19 +94,25 @@ def main():
     selected_dbs = select_db(pm.find_folders(pm.get_databases_dir()))
 
     for database in selected_dbs:
-        db = prepare_db(database, pm)
-        k_nn = knn(100, db.get_dataset_type(), db.get_classifier_col(), db.get_classifier_attr_cols())
-        # Run condensed nearest neighbor
-        cnn = k_nn.condensed_nn(db.get_data())
-        # Run edited nearest neighbor
-        # Training data, first 90%
-        td = db.get_data()[0:int(len(db.get_data()) * 0.9)]
-        # Validation Data, last 10%
-        vd = db.get_data()[int(len(db.get_data()) * 0.9):len(db.get_data())]
-        enn = k_nn.edited_knn(td, vd)
-        # Run data thru rbf net
-        rbf = RBF(len(enn), 7, 1)
-        rbf.predict(db.get_data(), enn)
+            db = prepare_db(database, pm)
+            k_nn = knn(100, db.get_dataset_type(), db.get_classifier_col(), db.get_classifier_attr_cols())
+            # Run condensed nearest neighbor
+            cnn = k_nn.condensed_nn(db.get_data())
+            # Run edited nearest neighbor
+            # Training data, first 90%
+            td = db.get_data()[0:int(len(db.get_data()) * 0.9)]
+            # Validation Data, last 10%
+            vd = db.get_data()[int(len(db.get_data()) * 0.9):len(db.get_data())]
+            enn = k_nn.edited_knn(td, vd)
+            # Run data thru rbf net
+            rbf = RBF(len(enn), 7, 1)
+
+            # Hardcoding the classes for now
+            #TODO implement a way to get classes thru database.py
+            classes = ['BRICKFACE', 'SKY', 'FOLIAGE', 'CEMENT', 'WINDOW', 'PATH', 'GRASS']
+            # get column vector storing correct classifications of each row
+            y = np.array(db.get_data())[:,db.get_classifier_col()]
+            rbf.fit(db.get_data(), enn, y, classes)
     
 # -------------------------------------------------------------
 
