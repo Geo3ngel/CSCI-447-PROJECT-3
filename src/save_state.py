086c3a6e7be15918a3f65d323cb588086fc63039
GeoEngel <GeoEngel.z@gmail.com>
2019-11-06 09:55:07 -0700

    Moved save state file generation and pathing out of main into save_state

--- src/save_state.py
+++ src/save_state.py
@@ -23,6 +23,45 @@ def save_state(state_object, state_filename):
     with open(state_filename, 'wb') as state_file:
         pickle.dump(state_object, state_file)
         
-# Make the folder for the current database's save states (if one does not already exist)
-def gen_save_folder():
-    pass
+# Set the path manager's current save folder to the current settings if it exists, or create it
+def verify_save_folder(pm, db):
+    save_folder_name = ""
+    for layer in db.get_layers():
+        save_folder_name+=str(layer)+"-"
+        
+    # Remove the last extra character
+    save_folder_name = save_folder_name[:-1]
+    
+    pm.set_save_state_folder(save_folder_name)
+    
+    print("FOLDER NAME:", pm.get_save_state_dir())
+    
+    pm.make_folder_at_dir(pm.get_save_state_dir())
+        
+    return select_save_state(pm)
+    
+
+# Allows you to load from a save state for a given database's layer/node combination if one is present.
+def select_save_state(pm):
+    # Checks that save state folder contains states
+    if len(pm.find_files(pm.get_save_state_dir(), "")) > 0:
+        
+        # Provides the option to load from an existing save state
+        awns = input("\nWould you like to load from an existing save state?")
+        if awns.lower() is "y":
+            exists = True
+            while(exists):
+                print("Current save states (Epochs):", pm.find_files(pm.get_save_state_dir(), ""))
+                save_state = input("Select a saved state (Epoch #):")
+                # Validate the save state exists
+                path = os.path.join(pm.get_save_state_dir(), save_state)
+                exists = pm.validate_file(path)
+                
+                if exists:
+                    # Load save_state object and return!
+                    return ss.load_state(path)
+                else:
+                    print("Invalid save state. Try again.")
+        else:
+            print("Beginning new Neural Net...")
+    return False
